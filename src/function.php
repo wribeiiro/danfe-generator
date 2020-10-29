92a4de74447f76b8e303ac066bcb016fd96f23a9
Wellisson Ribeiro <welleh10@gmail.com>
2020-10-29 14:19:38 -0300

    force .pdf download file

--- src/function.php
+++ src/function.php
@@ -43,7 +43,7 @@ function checkModelDocument(string $accessKey): int {
 }
 
 function printPdf(string $pdfName, string $pdf): void {
-    header("Content-Disposition: attachment; filename=$pdfName");
+    header("Content-Disposition: attachment; filename=$pdfName.pdf");
     header("Content-Type: application/pdf");
     echo($pdf);
 }
